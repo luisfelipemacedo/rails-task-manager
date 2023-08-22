28f16d192bb90317be0fe491a8c56511f4732516
Luis Macedo <lfelipe_macedo@hotmail.com>
2023-08-22 19:17:23 -0300

    exercicio 02 sem refatorar

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all # pega todos os tasks do DB
+  end
+
+  def new
+    @task = Task.new # instancia um novo task
+  end
+
+  def show
+    @task = Task.find(params[:id]) # vindo do browser
+  end
+
+  def create
+    @task = Task.new(task_params) # instancia com as caracteristicas permitidas
+    @task.save # para persistir os dados no banco de dados
+    redirect_to tasks_path(@task) # é opcional colocar o @task dentro do parenteses, pega pela id;
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    # redirect_to task_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  # faz isso para permitir criar com essas caracteristicas
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # verb 'path', to: 'controller#action'
+
+  # Read all tasks
+  get 'tasks', to: 'tasks#index', as: :tasks # fez essa assinatura para voltar para lista principal
+
+  # create a task
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+
+  # see one task
+  get 'tasks/:id', to: 'tasks#show', as: :task # passa o id dinamico na url dependendo da task
+
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
